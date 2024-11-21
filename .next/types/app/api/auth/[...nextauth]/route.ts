8eb9cfbcf44409b1f6c4d8792e4c198d58af19ea
Abigail Lorber <alorber@hawaii.edu>
2024-11-21 13:38:29 -1000

    added profile page

--- .next/types/app/api/auth/[...nextauth]/route.ts
+++ .next/types/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-// File: /Users/julian/Documents/GitHub/iCompanion/src/app/api/auth/[...nextauth]/route.ts
+// File: /Users/Abberts/Documents/GitHub/ICS 314/final project/iCompanion/src/app/api/auth/[...nextauth]/route.ts
 import * as entry from '../../../../../../src/app/api/auth/[...nextauth]/route.js'
 import type { NextRequest } from 'next/server.js'
 
