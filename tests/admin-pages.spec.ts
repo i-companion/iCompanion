ad031a64ace1eb0ee551c4bd8e66fad97c1ddf28
Abigail Lorber <alorber@hawaii.edu>
2024-12-06 15:07:14 -1000

    deleted Stuff

--- tests/admin-pages.spec.ts
+++ tests/admin-pages.spec.ts
@@ -7,15 +7,8 @@ test.use({
 test('Admin Pages', async ({ page }) => {
   await page.goto('http://localhost:3000/');
   await expect(page.getByRole('link', { name: 'Next.js Application Template' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'Add Stuff' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'List Stuff' })).toBeVisible();
   await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible();
   await expect(page.getByRole('button', { name: 'admin@foo.com' })).toBeVisible();
-  await page.getByRole('link', { name: 'Add Stuff' }).click();
-  await expect(page.getByRole('heading', { name: 'Add Stuff' })).toBeVisible();
-  await page.getByRole('link', { name: 'List Stuff' }).click();
-  await expect(page.getByRole('heading', { name: 'Stuff' })).toBeVisible();
   await page.getByRole('link', { name: 'Admin' }).click();
-  await expect(page.getByRole('heading', { name: 'List Stuff Admin' })).toBeVisible();
   await expect(page.getByRole('heading', { name: 'List Users Admin' })).toBeVisible();
 });
