92dba0992d6621c1f1d674095ccb94c4b3351120
JulianBejar <jbejar@hawaii.edu>
2024-12-10 08:37:17 -1000

    Updates 1

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -6,8 +6,6 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
 import { Card, Col, Container, Button, Form, Row } from 'react-bootstrap';
 import { createUser, createProfile } from '@/lib/dbActions';
-import { FieldArray, Formik } from "formik";
-
 
 type SignUpForm = {
   email: string;
@@ -18,57 +16,23 @@ type SignUpForm = {
   image: string;
   description: string;
   interests: boolean[];
-  /*
-  interests: {
-    valorant?: boolean;
-    leagueOfLegends?: boolean;
-    callOfDuty?: boolean;
-  };
-  */
-
 };
-
-/** The sign up page. */
 
 const SignUp = () => {
   const validationSchema = Yup.object().shape({
-
     email: Yup.string().required('Email is required').email('Email is invalid'),
-
     password: Yup.string()
       .required('Password is required')
       .min(6, 'Password must be at least 6 characters')
       .max(40, 'Password must not exceed 40 characters'),
-
     confirmPassword: Yup.string()
       .required('Confirm Password is required')
       .oneOf([Yup.ref('password'), ''], 'Confirm Password does not match'),
-
     name: Yup.string().required('Name is required'),
-
     discord: Yup.string().required('Discord handle is required'),
-
     image: Yup.string().required('Image is required'),
-
     description: Yup.string().required('Description is required'),
-
-////////////////////////////////////////////////////////////
-//
-// the interests below iss what i think is the issue
-///
-
     interests: Yup.array().min(1).of(Yup.bool().required()).required('At least one interest is required'),
-
-
-    /*
-    interests: Yup.object().shape({
-      valorant: Yup.boolean(),
-      leagueOfLegends: Yup.boolean(),
-      callOfDuty: Yup.boolean(),
-    }),
-    */
-
-
   });
 
   const {
@@ -81,12 +45,8 @@ const SignUp = () => {
   });
 
   const onSubmit = async (data: SignUpForm) => {
-    // calls function that creates a user in the database
     await createUser(data);
-    
-    // calls function that creates a profile in the database
     await createProfile(data);
-
     await signIn('credentials', { callbackUrl: '/profile', ...data });
   };
 
@@ -99,18 +59,16 @@ const SignUp = () => {
             <Card>
               <Card.Body>
                 <Form onSubmit={handleSubmit(onSubmit)}>
-                  {/* Email */}
                   <Form.Group className="form-group">
                     <Form.Label>Email</Form.Label>
                     <input
-                      type="text"
+                      type="email"
                       {...register('email')}
                       className={`form-control ${errors.email ? 'is-invalid' : ''}`}
                     />
                     <div className="invalid-feedback">{errors.email?.message}</div>
                   </Form.Group>
 
-                  {/* Password */}
                   <Form.Group className="form-group">
                     <Form.Label>Password</Form.Label>
                     <input
@@ -121,7 +79,6 @@ const SignUp = () => {
                     <div className="invalid-feedback">{errors.password?.message}</div>
                   </Form.Group>
 
-                  {/* Confirm Password */}
                   <Form.Group className="form-group">
                     <Form.Label>Confirm Password</Form.Label>
                     <input
@@ -132,7 +89,6 @@ const SignUp = () => {
                     <div className="invalid-feedback">{errors.confirmPassword?.message}</div>
                   </Form.Group>
 
-                  {/* Name */}
                   <Form.Group className="form-group">
                     <Form.Label>Name</Form.Label>
                     <input
@@ -143,8 +99,6 @@ const SignUp = () => {
                     <div className="invalid-feedback">{errors.name?.message}</div>
                   </Form.Group>
 
-
-                  {/* Discord */}
                   <Form.Group className="form-group">
                     <Form.Label>Discord</Form.Label>
                     <input
@@ -155,7 +109,6 @@ const SignUp = () => {
                     <div className="invalid-feedback">{errors.discord?.message}</div>
                   </Form.Group>
 
-                  {/* Image */}
                   <Form.Group className="form-group">
                     <Form.Label>Image</Form.Label>
                     <input
@@ -166,7 +119,6 @@ const SignUp = () => {
                     <div className="invalid-feedback">{errors.image?.message}</div>
                   </Form.Group>
 
-                  {/* Description */}
                   <Form.Group className="form-group">
                     <Form.Label>Description</Form.Label>
                     <input
@@ -177,14 +129,12 @@ const SignUp = () => {
                     <div className="invalid-feedback">{errors.description?.message}</div>
                   </Form.Group>
 
-                  {/* Interests */}
-
-                  <Form.Group name="interests" className="form-group">
+                  <Form.Group className="form-group">
                     <Form.Label>Interests</Form.Label>
                     <div className="form-check">
                       <input
                         type="checkbox"
-                        {...register('interests')}
+                        {...register('interests[0]')}
                         className="form-check-input"
                       />
                       <label className="form-check-label">Valorant</label>
@@ -192,7 +142,7 @@ const SignUp = () => {
                     <div className="form-check">
                       <input
                         type="checkbox"
-                        {...register('interests')}
+                        {...register('interests[1]')}
                         className="form-check-input"
                       />
                       <label className="form-check-label">League of Legends</label>
@@ -200,15 +150,45 @@ const SignUp = () => {
                     <div className="form-check">
                       <input
                         type="checkbox"
-                        {...register('interests')}
+                        {...register('interests[2]')}
                         className="form-check-input"
                       />
                       <label className="form-check-label">Call of Duty</label>
                     </div>
+                    <div className="form-check">
+                      <input
+                        type="checkbox"
+                        {...register('interests[3]')}
+                        className="form-check-input"
+                      />
+                      <label className="form-check-label">Overwatch</label>
+                    </div>
+                    <div className="form-check">
+                      <input
+                        type="checkbox"
+                        {...register('interests[4]')}
+                        className="form-check-input"
+                      />
+                      <label className="form-check-label">Genshin Impact</label>
+                    </div>
+                    <div className="form-check">
+                      <input
+                        type="checkbox"
+                        {...register('interests[5]')}
+                        className="form-check-input"
+                      />
+                      <label className="form-check-label">Super Smash Bros</label>
+                    </div>
+                    <div className="form-check">
+                      <input
+                        type="checkbox"
+                        {...register('interests[6]')}
+                        className="form-check-input"
+                      />
+                      <label className="form-check-label">Apex Legends</label>
+                    </div>
                   </Form.Group>
 
-
-                  {/* Buttons */}
                   <Form.Group className="form-group py-3">
                     <Row>
                       <Col>
