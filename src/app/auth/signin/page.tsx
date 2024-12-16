dfaf072da67ab894f76fa6d911628a68e260eb65
JulianBejar <jbejar@hawaii.edu>
2024-12-15 21:20:33 -1000

    Update page.tsx

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -42,7 +42,7 @@ const SignIn = () => {
                     <input name="password" type="password" className="form-control" />
                   </Form.Group>
                   <Button type="submit" className="mt-3">
-                    Signin
+                    Sign in
                   </Button>
                 </Form>
                 <div className="mt-auto pt-3 text-center custom-text">
