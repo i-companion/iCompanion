3dee631f4f41fb472cc98cef90fea6d6a2b46d80
Kai Sutton <157784335+kaibutton@users.noreply.github.com>
2024-12-15 19:00:02 -1000

    Update profile in database
    The profile table in the Vercel database now updates with the user's information when you sign up with a new user

--- src/lib/dbActions.ts
+++ src/lib/dbActions.ts
@@ -8,16 +8,29 @@ import { prisma } from './prisma';
  * Creates a new user in the database.
  * @param credentials, an object with the following properties: email, password.
  */
-export async function createUser(credentials: { email: string; password: string }) {
+export async function createUser(credentials: { email: string; password: string; confirmPassword: string; firstName: string; lastName: string; discord: string; interests: { valorant?: boolean; leagueOfLegends?: boolean; callOfDuty?: boolean; } }) {
   try {
     const hashedPassword = await hash(credentials.password, 10);
 
-    await prisma.user.create({
+    const newUser = await prisma.user.create({
       data: {
         email: credentials.email,
         password: hashedPassword,
       },
     });
+
+    await prisma.profile.create({
+      data: {
+        user: {
+          connect: { id: newUser.id }, // Connect the profile to the newly created user
+        },
+        name: credentials.firstName + ' ' + credentials.lastName,
+        discord: credentials.discord,
+        email: credentials.email,
+        description: "Add description",
+      },
+    });
+
   } catch (error) {
     console.error('Error creating user:', error);
     throw new Error('Failed to create user');
